7cb6680d115b50273883022bbd30ce02c82693eb
AOUGASSA VIGNO <AOUGASSA VIGNO@AOUGASSAVIGNO>
2017-03-26 23:04:29 +0200

    deuxieme modification C_1

--- Commande/src/commande/Commande.java
+++ Commande/src/commande/Commande.java
@@ -1,3 +1,4 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,12 +12,37 @@ package commande;
  * @author AOUGASSA VIGNO
  */
 public class Commande {
-
+     
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
     }
+   
     
+    {
+ 
+
+    int tableauEntier[][]= {{1,3,7,5,9,0},{6,9,7,5}},i=0,j=0;
+    while(i<6)
+    {
+        j=0;
+    while(j<5)
+    {
+        System.out.print(tableauEntier[i][j]);
+        j++;
+    }
+    System.out.println(" ");
+    i++;
+    }
+    }
 }
+
+
+    
+
+
+    
+
+
